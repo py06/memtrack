66db3f247912687b1953f7ac7e3978f8a343fad2
Pierre-Yves Kerbrat <pkerbrat@free.fr>
2017-03-30 19:52:31 +0200

    memtrack: make use of newly created process class
    Signed-off-by: Pierre-Yves Kerbrat <pkerbrat@free.fr>

--- memtrack.py
+++ memtrack.py
@@ -1,56 +1,32 @@
+#!/usr/bin/python
 import argparse
 import sys
 import hashlib
 import time
+from process import process
 from memarea import memarea
 
-
-areas = []
-maps_md5 = ""
-
-
-def parse_line(line):
-    line = ' '.join(line.split())
-    fields = line.strip(" +").split(" ")
-    base = fields[0].split("-")[0]
-    end = fields[0].split("-")[1]
-    perm = fields[1]
-    offset = fields[2]
-    major = fields[3].split(":")[0]
-    minor = fields[3].split(":")[1]
-    inode = fields[4]
-    if len(fields) > 5:
-        name = fields[5]
-    else:
-        name = "unknown"
-    new = memarea(base, end, perm, inode, name, offset=offset, major=major, minor=minor)
-    return new
 
 def md5_maps(pid):
     return hashlib.md5(open("/proc/"+pid+"/maps", 'r').read()).hexdigest()
 
-def read_maps(pid):
-    global areas
-
-    with open("/proc/"+pid+"/maps", 'r') as file:
-        for line in file:
-            line = line.rstrip()
-            areas.append(parse_line(line))
-
 def memtrack(pid):
-    global maps_md5
-
     print "Tracking memory usage of process " +pid
+    proc = process(pid)
 
     while (1):
-        md5 = md5_maps(pid)
-        if md5 != maps_md5:
-            read_maps(pid);
+        md5 = md5_maps(pid) #compute md5sum on /proc/<pid>/maps and
+                            #see if antyhing changed
+        if md5 != proc.get_md5():
+            memmap = proc.parse_maps(pid);
+            # do things with areas
 
-            for i in range(len(areas)):
-                    areas[i].display()
+            # commit them in proc object
+            proc.set_memareas(memmap)
+            proc.set_md5(md5_maps(pid))
+            for i in range(len(memmap)):
+                    memmap[i].display()
 
-            maps_md5 = md5;
         time.sleep(1)
 
     return 0
@@ -61,7 +37,7 @@ def memtrack(pid):
 def check_arg(args=None):
     parser = argparse.ArgumentParser(description='memtrack: process memory tracker')
     parser.add_argument('-p', '--pid', help='process id (pid)', required='True')
-    
+
     results = parser.parse_args(args)
     return (results.pid)
 
