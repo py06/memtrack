0f1c4d6edb85de388fe3c9534563853828dbe389
Pierre-Yves Kerbrat <pkerbrat@free.fr>
2017-03-30 16:33:55 +0200

    memarea: add pgoffset and major number
    Signed-off-by: Pierre-Yves Kerbrat <pkerbrat@free.fr>

--- memarea.py
+++ memarea.py
@@ -1,12 +1,15 @@
 #-*- coding: utf-8 -*-
 
 class memarea:
-	def __init__(self, base, end, perm, inode, name):
+	def __init__(self, base, end, perm, inode, name, offset, major, minor):
 		self.name = name;
 		self.base = base;
 		self.end = end;
 		self.perm = perm;
 		self.inode = inode;
+		self.offset = offset;
+		self.dev_maj = major;
+		self.dev_min = minor;
 	
 	def display(self):
 		print "Memory range: base = @"+self.base+" end = @"+self.end
