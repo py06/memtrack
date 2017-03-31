480e408bc0033d056d0f6449c21ff1cb35dcffe0
Pierre-Yves Kerbrat <pkerbrat@free.fr>
2017-03-31 15:47:28 +0200

    process: parse pagemap of process
    Signed-off-by: Pierre-Yves Kerbrat <pkerbrat@free.fr>

--- process.py
+++ process.py
@@ -1,6 +1,8 @@
 #-*- coding: utf-8 -*-
 import sys
+import struct
 from memarea import memarea
+from page import page
 
 class process:
 
@@ -34,6 +36,18 @@ class process:
                 memmap.append(self._parse_line(line))
         return memmap
 
+    def parse_pagemap(self, pid, start, count):
+        pagemap = []
+	offset  = (start / 4096) * 8
+        with open("/proc/"+pid+"/pagemap", 'r') as file:
+		file.seek(offset, 0)
+		for i in range(1, count):
+			entry = struct.unpack('Q', file.read(8))[0]
+			if not entry:
+				break;
+	                pagemap.append(page(entry))
+        return pagemap
+
     def get_md5(self):
         return self.maps_md5
 
@@ -43,8 +57,14 @@ class process:
     def get_memareas(self):
         return self.memareas
 
+    def get_pagemap(self):
+        return self.pagemap
+
     def set_md5(self, md5):
         self.maps_md5 = md5
 
     def __init__(self, pid):
-        self.areas = self.set_memareas(self.parse_maps(pid));
+        self.areas = self.parse_maps(pid);
+	start = 0
+	count = 10
+        self.pagemap = self.parse_pagemap(pid, start, count);
